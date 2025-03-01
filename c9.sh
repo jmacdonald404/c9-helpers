97276c9571d0c8f062a343d4e9c0d84303bc9930
Jeff MacDonald <jmacdonald404@gmail.com>
2025-03-01 12:06:31 -0800

    add fly cli

--- c9.sh
+++ c9.sh
@@ -2,10 +2,13 @@
 sudo apt-get update
 yes | sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev postgresql libpq-dev
 git clone https://github.com/excid3/asdf.git ~/.asdf
+curl -L https://fly.io/install.sh | sh
 echo '. "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
 echo '. "$HOME/.asdf/completions/asdf.bash"' >> ~/.bashrc
 echo 'legacy_version_file = yes' >> ~/.asdfrc
 echo 'export EDITOR="code --wait"' >> ~/.bashrc
+echo 'export FLYCTL_INSTALL="$HOME/.fly"' >> ~/.bashrc
+echo 'export PATH="$FLYCTL_INSTALL/bin:$PATH"' >> ~/bashrc
 source ~/.bashrc
 source ~/.asdfrc
 asdf plugin add ruby
